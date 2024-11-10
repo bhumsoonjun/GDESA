9cd61c776f35ff466cf9280e80952847a4556da8
bhumsoonjun <83196403+bhumsoonjun@users.noreply.github.com>
2024-11-10 10:50:33 +0700

    Refac

--- base_gdesa.py
+++ base_gdesa.py
@@ -9,6 +9,8 @@ import scipy.optimize as opt
 import scipy.stats as stats
 from abc import ABC, abstractmethod
 
+from utils import latin
+
 
 class BaseGDESA(ABC):
 
@@ -31,7 +33,6 @@ class BaseGDESA(ABC):
         self.best_fitness = None
         self.cooldown_list = None
 
-        self.sampler = None
         self.seed = 0
         self.nfev = 0
         pass
@@ -45,41 +46,6 @@ class BaseGDESA(ABC):
             jac="2-point",
         )
         return opt_res.x, opt_res.fun
-
-    def check_bounds(self, x: np.ndarray) -> np.ndarray:
-        x_copy = copy.deepcopy(x)
-        lb = [b[0] for b in self.bounds]
-        ub = [b[1] for b in self.bounds]
-        x_copy = np.clip(x_copy, lb, ub)
-        return x_copy
-
-    def latin(self, n: int, bounds: List[Tuple]) -> np.ndarray:
-        sample = self.sampler.random(n=n)
-        bounded = np.apply_along_axis(lambda x: self.check_bounds(x), 1, sample)
-        return np.array(bounded, dtype=np.float64)
-
-
-    def dual_mutation(self, j: int) -> np.ndarray:
-        if np.random.rand() < 0.5:
-            mutant_vector = self.rand_1_bin_mutator(
-                self.population, self.mutation_factor, j
-            )
-        else:
-            mutant_vector = self.rand_2_bin_mutator(
-                self.population, self.mutation_factor, j
-            )
-        return mutant_vector
-
-    def binomial_crossover(
-        self, target_vector: np.ndarray, mutant_vector: np.ndarray
-    ) -> np.ndarray:
-        trial = np.copy(target_vector)
-        fill_point = np.random.randint(0, target_vector.shape[0])
-        crossovers = np.random.uniform(size=target_vector.shape)
-        crossovers = crossovers < self.crossover_prob
-        crossovers[fill_point] = True
-        trial = np.where(crossovers, mutant_vector, trial)
-        return trial
 
     def func_wrapper(self, x: np.ndarray) -> float:
         self.nfev += 1
@@ -115,8 +81,7 @@ class BaseGDESA(ABC):
         self.lb = [b[0] for b in self.bounds]
         self.ub = [b[1] for b in self.bounds]
 
-        self.sampler = stats.qmc.LatinHypercube(d=len(self.bounds), seed=self.seed)
-        self.population = self.latin(self.popsize, self.bounds)
+        self.population = latin(self.popsize, self.bounds, self.seed)
         self.population_fitness = np.apply_along_axis(
             self.func_wrapper, 1, self.population
         )
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
+import copy
+from typing import *
 import numpy as np
+from scipy import stats
+
 
 def rand_1_bin_mutator(
-        population: np.ndarray, differential_factor: float, i: int
+    population: np.ndarray, differential_factor: float, i: int
 ) -> np.ndarray:
     j, k, l = 0, 0, 0
     while j == i:
@@ -11,13 +15,13 @@ def rand_1_bin_mutator(
     while l == i or l == j or l == k:
         l = np.random.randint(0, population.shape[0])
     mutant_vector = population[j] + differential_factor * (
-            population[k] - population[l]
+        population[k] - population[l]
     )
     return mutant_vector
 
 
 def rand_2_bin_mutator(
-        population: np.ndarray, differential_factor: float, i: int
+    population: np.ndarray, differential_factor: float, i: int
 ) -> np.ndarray:
     j, k, l, m, n = 0, 0, 0, 0, 0
     while j == i:
@@ -31,17 +35,39 @@ def rand_2_bin_mutator(
     while n == i or n == j or n == k or n == l or n == m:
         n = np.random.randint(0, population.shape[0])
     mutant_vector = population[j] + differential_factor * (
-            population[k] - population[l] + population[m] - population[n]
+        population[k] - population[l] + population[m] - population[n]
     )
     return mutant_vector
 
+
 def dual_mutation(population: np.ndarray, mutation_factor: float, i: int) -> np.ndarray:
-    if np.random.rand() < 0.5:
-        mutant_vector = rand_1_bin_mutator(
-            population, mutation_factor, i
-        )
-    else:
-        mutant_vector = rand_2_bin_mutator(
-            population, mutation_factor, i
-        )
-    return mutant_vector
+    return (
+        rand_1_bin_mutator(population, mutation_factor, i)
+        if np.random.rand() < 0.5
+        else rand_2_bin_mutator(population, mutation_factor, i)
+    )
+
+def binomial_crossover(
+    target_vector: np.ndarray,
+    mutant_vector: np.ndarray,
+    crossover_prob: float
+) -> np.ndarray:
+    trial = np.copy(target_vector)
+    fill_point = np.random.randint(0, target_vector.shape[0])
+    crossovers = np.random.uniform(size=target_vector.shape)
+    crossovers = crossovers < crossover_prob
+    crossovers[fill_point] = True
+    trial = np.where(crossovers, mutant_vector, trial)
+    return trial
+
+def check_bounds(x: np.ndarray, bounds: List[Tuple]) -> np.ndarray:
+    x_copy = copy.deepcopy(x)
+    lb = [b[0] for b in bounds]
+    ub = [b[1] for b in bounds]
+    x_copy = np.clip(x_copy, lb, ub)
+    return x_copy
+
+def latin(n: int, bounds: List[Tuple], seed: int) -> np.ndarray:
+    sample = stats.qmc.LatinHypercube(d=len(bounds), seed=seed).random(n=n)
+    bounded = np.apply_along_axis(lambda x: check_bounds(x, bounds), 1, sample)
+    return np.array(bounded, dtype=np.float64)
